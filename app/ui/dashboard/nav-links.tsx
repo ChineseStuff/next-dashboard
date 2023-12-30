22aa41a6a0cf6d08030e1f0172612eb411f27b6b
Pablo Navarro <pablon.88@gmail.com>
2023-12-30 02:36:10 -0300

    Routing and Navigation

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,3 +1,9 @@
+'use client';
+
+import clsx from 'clsx';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import {
   UserGroupIcon,
   HomeIcon,
@@ -17,19 +23,27 @@ const links = [
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname();
+
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
+        const linkClassNames = clsx(
+          'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+          {
+            'bg-sky-100 text-blue-600': link.href === pathname
+          }
+        )
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className={linkClassNames}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
